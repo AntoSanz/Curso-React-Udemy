9a97eb32b5d9b55673ecadb4c73252773888bc3f
Antonio <aspans@gmail.com>
2023-06-26 12:05:05 +0200

    37-Fragment

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-
 export const FirstApp = () => {
-  return (
-    <h2>First App</h2>
-  )
-}
+    return (
+        <>  
+        {/* <></> es lo mismo que <Fragment></Fragment> y no es necesario importar el Fragment*/}
+            <h2>First App</h2>
+            <p>My first App</p>
+        </>
+    );
+};
