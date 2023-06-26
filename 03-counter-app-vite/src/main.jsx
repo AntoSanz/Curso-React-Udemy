5530d8a4fe1096fc3da051ddc3b27857691a2c29
Antonio <aspans@gmail.com>
2023-06-26 12:33:05 +0200

    39-Add CSS style

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelloWorldApp } from "./helloWorldApp";
+// import { HelloWorldApp } from "./helloWorldApp";
 import { FirstApp } from "./FirstApp";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
