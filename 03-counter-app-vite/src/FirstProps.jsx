e307d0c635c653744c573cced294f8d6677de151
Antonio <aspans@gmail.com>
2023-06-26 12:47:29 +0200

    40-Props

---
+++ 03-counter-app-vite/src/FirstProps.jsx
@@ -0,0 +1,6 @@
+//rafc = Short cut para crear un componente
+
+export const FirstProps = ({title, subTitle}) => {
+    console.log(title);
+    console.log(subTitle + 1);
+}
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 // import { HelloWorldApp } from "./helloWorldApp";
 import { FirstApp } from "./FirstApp";
 import "./styles.css";
+import { FirstProps } from "./FirstProps";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         {/* <HelloWorldApp /> */}
         <FirstApp />
+        <FirstProps title="Hola soy Anto" subTitle={123} />
     </React.StrictMode>
 );
