a463692b15440c6753886a02cda2bc79869148f4
Antonio <aspans@gmail.com>
2023-09-11 10:56:50 +0200

    57. toEqual y toStrictEqual

--- 03-counter-app-vite/test/base-pruebas/02-template-string.test.js
+++ 03-counter-app-vite/test/base-pruebas/02-template-string.test.js
@@ -1,6 +1,6 @@
 import { getSaludo } from "../../src/base-pruebas/base-pruebas/02-template-string";
 
-describe('pruebas en <02-template-string />', () => { 
+describe('Pruebas en <02-template-string />', () => { 
     test('getSaludo debe devolver "Hola Antonio"', () =>{
         const name = 'Antonio';
         const message = getSaludo(name);
