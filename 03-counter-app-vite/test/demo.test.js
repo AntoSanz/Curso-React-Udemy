74c69c87ab7a9edd67ff6e85c35172441f3d6068
Antonio <aspans@gmail.com>
2023-10-02 16:47:29 +0200

    fix

--- 03-counter-app-vite/test/demo.test.js
+++ 03-counter-app-vite/test/demo.test.js
@@ -7,5 +7,5 @@ describe('Pruebas en <DemoComponent />', () => {
         const message1 = 'Hola mundo';
         const message2 = message1.trim();
         expect(message1).toBe(message2);
-    })
- })
+    });
+ });
