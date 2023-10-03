e5454a619323c8b439705b197db1eaf54893f653
Antonio <aspans@gmail.com>
2023-10-03 17:29:42 +0200

    69. Simular eventos - Click

--- 03-counter-app-vite/test/CounterApp.test.jsx
+++ 03-counter-app-vite/test/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { CounterApp } from '../src/CounterApp';
 
 describe('Pruebas en <CounterApp />', () => {
@@ -11,6 +11,25 @@ describe('Pruebas en <CounterApp />', () => {
 
     test('Debe mostrar el valor inicial de 100', () => {
         render(<CounterApp value={initialValue} />);
+        expect(screen.getByText(initialValue)).toBeTruthy();
+    });
+
+    test('Debe de incrementar con el boton +1', () => {
+        render(<CounterApp value={initialValue} />);
+        fireEvent.click(screen.getByText('+1'));
+        expect(screen.getByText(initialValue + 1)).toBeTruthy();
+    });
+
+    test('Debe de reducir con el valor -1', () => {
+        render(<CounterApp value={initialValue} />);
+        fireEvent.click(screen.getByText('-1'));
+        expect(screen.getByText(initialValue - 1)).toBeTruthy();
+    });
+
+    test('El boton de reset debe restaurar el valor inicial', () => {
+        render(<CounterApp value={initialValue} />);
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByRole('button', { name: 'btn-reset'}));
         expect(screen.getByText(initialValue)).toBeTruthy();
     });
 });
