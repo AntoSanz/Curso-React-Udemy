4cc89e558261d82e9a9155c163e635a6db94068c
Antonio <aspans@gmail.com>
2023-10-04 17:24:18 +0200

    77. GitExpertApp - Añadir nueva categoria

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,11 +3,17 @@ import { useState } from 'react';
 export const GifExpertApp = () => {
     //useStateSnippet
     const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
-    console.log(categories);
+
+    const onAddCategory = () => {
+        let newCategory = 'One Piece';
+        // setCategories(c => [c, newCategory]);
+        setCategories([...categories, newCategory]);
+    };
 
     return (
         <>
             <h1>GifExpertApp</h1>
+            <button onClick={onAddCategory}>Añadir</button>
             <ol>
                 {categories.map((category) => {
                     return <li key={category}>{category}</li>;
