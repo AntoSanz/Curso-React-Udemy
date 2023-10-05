f17c68ec5672117bff83681c79defb5492e01a7c
Antonio <aspans@gmail.com>
2023-10-05 13:53:52 +0200

    83. Fetch API - Obtener imagenes deseadas

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,8 +1,13 @@
-export const GifGrid = (category) => {
+import { getGifs } from "../helpers/getGifs";
+
+export const GifGrid = ({ category }) => {
+
+    //TODO: MALA PRACTICA QUE RESOLVER
+    getGifs(category);
+
     return (
         <>
             <h3>{category}</h3>
-            <p>Hola Mundo</p>
         </>
     );
 };
